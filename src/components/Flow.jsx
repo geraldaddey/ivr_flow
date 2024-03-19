118ec34cfbdb906a588aed70e01aa2693cbc4db8
GERALD ADDEY <geraldaddey@yahoo.com>
2024-03-19 13:09:26 +0000

    feat: create custom salesforce node

---
+++ src/components/Flow.jsx
@@ -0,0 +1,75 @@
+import React, { useCallback, useMemo } from "react";
+import ReactFlow, {
+  addEdge,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+} from "reactflow";
+
+import {
+  nodes as initialNodes,
+  edges as initialEdges,
+} from "../initial_element";
+import Salesforce from "./Salesforce";
+import CallerIntent from "./CallerIntent";
+
+import "reactflow/dist/style.css";
+import "../overview.css";
+//
+// const nodeTypes = {
+//   custom: Salesforce,
+// };
+
+const minimapStyle = {
+  height: 120,
+};
+
+const onInit = (reactFlowInstance) =>
+  console.log("flow loaded:", reactFlowInstance);
+
+const OverviewFlow = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  console.log(nodes, edges);
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    []
+  );
+
+  const nodeTypes = useMemo(() => ({ custom: Salesforce }), []);
+
+  // we are using a bit of a shortcut here to adjust the edge type
+  // this could also be done with a custom edge for example
+  const edgesWithUpdatedTypes = edges.map((edge) => {
+    if (edge.sourceHandle) {
+      console.log("Nodes :\n", nodes);
+      const edgeType = nodes.find((node) => node.type === "custom").data
+        .selects[edge.sourceHandle];
+      edge.type = edgeType;
+    }
+    return edge;
+  });
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edgesWithUpdatedTypes}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      onInit={onInit}
+      fitView
+      attributionPosition="top-right"
+      nodeTypes={nodeTypes}
+    >
+      <MiniMap style={minimapStyle} zoomable pannable />
+      <Controls />
+      <Background color="#aaa" gap={16} />
+    </ReactFlow>
+  );
+};
+
+export default OverviewFlow;
